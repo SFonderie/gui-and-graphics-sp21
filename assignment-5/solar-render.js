4cbf6a0ecb3e9324e583563a516ab09bce3ce1a0
SFonderie <sydney3449@gmail.com>
2021-02-05 23:12:18 -0800

    Solar System
    Assignment 5 Code

---
+++ assignment-5/solar-render.js
@@ -0,0 +1,212 @@
+/////////////////////////////////////////////////////////////////////////////
+//
+//  Solar.js
+//
+/////////////////////////////////////////////////////////////////////////////
+
+var canvas;
+var gl;
+
+//---------------------------------------------------------------------------
+//
+//  Declare our array of planets (each of which is a sphere)
+//
+// The list of planets to render.  Uncomment any planets that you are 
+// including in the scene. For each planet in this list, make sure to 
+// set its distance from the Sun, as well its size, color, and orbit
+// around the Sun. 
+
+var Planets = {
+  Sun : undefined,
+  Mercury : undefined,
+  Venus : undefined,
+  Earth : undefined,
+  Moon : undefined,
+  Mars : undefined,
+  Jupiter : undefined,
+  Saturn : undefined,
+  Uranus : undefined,
+  Neptune : undefined,
+  Pluto : undefined
+};
+
+// Viewing transformation parameters
+var V;  // matrix storing the viewing transformation
+
+// Projection transformation parameters
+var P;  // matrix storing the projection transformation
+var near = 5;      // near clipping plane's distance
+var far = 150;      // far clipping plane's distance
+
+// Animation variables
+var time = 0.0;      // time, our global time constant, which is 
+                     // incremented every frame
+var timeDelta = 0.5; // the amount that time is updated each frame
+
+//---------------------------------------------------------------------------
+//
+//  init() - scene initialization function
+//
+
+function init() {
+  canvas = document.getElementById("webgl-canvas");
+
+  // Configure our WebGL environment
+  gl = WebGLUtils.setupWebGL(canvas);
+  if (!gl) { alert("WebGL initialization failed"); }
+
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.enable(gl.DEPTH_TEST);
+
+  // Initialize the planets in the Planets list, including specifying
+  // necesasry shaders, shader uniform variables, and other initialization
+  // parameters.  This loops adds additinoal properties to each object
+  // in the Planets object;
+
+  for (var name in Planets ) {
+
+    // Create a new sphere object for our planet, and assign it into the
+    // appropriate place in the Planets dictionary.  And to simplify the code
+    // assign that same value to the local variable "p", for later use.
+
+    var planet = Planets[name] = new Sphere();
+
+    // For each planet, we'll add a new property (which itself is a 
+    // dictionary) that contains the uniforms that we will use in
+    // the associated shader programs for drawing the planets.  These
+    // uniform's values will be set each frame in render().
+
+    planet.uniforms = { 
+      color : gl.getUniformLocation(planet.program, "color"),
+      MV : gl.getUniformLocation(planet.program, "MV"),
+      P : gl.getUniformLocation(planet.program, "P"),
+    };
+  }
+
+  resize();
+
+  window.requestAnimationFrame(render);  
+}
+
+//---------------------------------------------------------------------------
+//
+//  render() - render the scene
+//
+
+function render() 
+{
+	time += timeDelta;
+	
+	var ms = new MatrixStack();
+	
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	
+	V = translate(0.0, 0.0, -0.5*(near + far));
+	ms.load(V);
+	
+	var sun = Planets["Sun"];
+	var data = SolarSystem["Sun"];
+	
+	sun.PointMode = false;
+	
+	ms.push();
+	ms.scale(data.radius);
+	gl.useProgram(sun.program);
+	gl.uniformMatrix4fv(sun.uniforms.MV, false, flatten(ms.current()));
+	gl.uniformMatrix4fv(sun.uniforms.P, false, flatten(P));
+	gl.uniform4fv(sun.uniforms.color, flatten(data.color));
+	sun.render();
+	
+	// NOW THE ACTUAL PLANETS
+	RenderPlanet(ms, "Mercury");
+	RenderPlanet(ms, "Venus");
+	RenderPlanet(ms, "Earth", ["Moon"]);
+	RenderPlanet(ms, "Mars");
+	RenderPlanet(ms, "Jupiter");
+	RenderPlanet(ms, "Saturn");
+	RenderPlanet(ms, "Uranus");
+	RenderPlanet(ms, "Neptune");
+	RenderPlanet(ms, "Pluto");
+	
+	ms.pop();
+	
+	window.requestAnimationFrame(render);
+}
+
+function RenderPlanet(ms, name, moons)
+{
+	var planet = Planets[name];
+	var data = SolarSystem[name];
+	planet.PointMode = false;
+	
+	// Up the scope.
+	ms.push();
+	
+	// Actual matrix stuff
+	ms.rotate((1.0 / data.year) * time, [0.0, 1.0, 1.0]);
+	ms.translate(data.distance, 0, 0);
+	ms.scale(data.radius);
+	
+	// Rendering stuff
+	gl.useProgram(planet.program);
+	gl.uniformMatrix4fv(planet.uniforms.MV, false, flatten(ms.current()));
+	gl.uniformMatrix4fv(planet.uniforms.P, false, flatten(P));
+	gl.uniform4fv(planet.uniforms.color, flatten(data.color));
+	planet.render();
+	
+	if(moons)
+	{
+		for(var i = 0; i < moons.length; i++)
+		{
+			RenderMoon(ms, moons[i]);
+		}
+	}
+	
+	// Drop the scope.
+	ms.pop();
+}
+
+function RenderMoon(ms, name)
+{
+	var moon = Planets[name];
+	var data = SolarSystem[name];
+	moon.PointMode = false;
+	
+	// Up the scope.
+	ms.push();
+	
+	// Actual matrix stuff
+	ms.rotate((1.0 / data.year) * time, [0.0, 1.0, 1.0]);
+	ms.translate(data.distance, 0, 0);
+	ms.scale(data.radius);
+	
+	// Rendering stuff
+	gl.useProgram(moon.program);
+	gl.uniformMatrix4fv(moon.uniforms.MV, false, flatten(ms.current()));
+	gl.uniformMatrix4fv(moon.uniforms.P, false, flatten(P));
+	gl.uniform4fv(moon.uniforms.color, flatten(data.color));
+	moon.render();
+	
+	// Drop the scope.
+	ms.pop();
+}
+
+function resize() {
+  var w = canvas.clientWidth;
+  var h = canvas.clientHeight;
+
+  gl.viewport(0, 0, w, h);
+
+  var fovy = 90.0; // degrees
+  var aspect = w / h;
+
+  P = perspective(fovy, aspect, near, far);
+}
+
+//---------------------------------------------------------------------------
+//
+//  Window callbacks for processing various events
+//
+
+window.onload = init;
+window.onresize = resize;
---
+++ assignment-5/solar-system.js
@@ -0,0 +1,68 @@
+var SolarSystem = {
+  Sun : {
+    radius : 3,  
+    distance : 0,
+    year : 0,
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  Mercury : {
+    radius : 0.1,
+    distance : 1.5,
+    year : 0.25,
+    color : [ 0.6, 0.3, 0.0, 1.0 ]
+  },
+  Venus : {
+    radius : 0.36,
+    distance : 3,
+    year : 0.6,
+    color : [ 1.0, 0.75, 0.0, 1.0 ]
+  },
+  Earth : {
+    radius : 0.4,
+    distance : 4.5,
+    year : 1,
+    color : [ 0.0, 0.5, 1.0, 1.0 ]
+  },
+  Moon : {
+    radius : 0.2,
+    distance : 1.5,
+    year : 0.075,
+    color : [ 0.5, 0.5, 0.5, 1.0 ]
+  },
+  Mars : {
+    radius : 0.25,
+    distance : 6,
+    year : 1.8,
+    color : [ 1.0, 0.0, 0.0, 1.0 ]
+  },
+  Jupiter : {
+    radius : 0.8,
+    distance : 9,
+    year : 2.5,
+    color : [ 1.0, 153/255, 0.0, 1.0 ]
+  },
+  Saturn : {
+    radius : 0.6,
+    distance : 12,
+    year : 3,
+    color : [ 1.0, 1.0, 0.0, 1.0 ]
+  },
+  Uranus : {
+    radius : 0.5,
+    distance : 15,
+    year : 4.2,
+    color : [ 0.0, 0.8, 0.8, 1.0 ]
+  },
+  Neptune : {
+    radius : 0.5,
+    distance : 18,
+    year : 5,
+    color : [ 0.0, 0.0, 1.0, 1.0 ]
+  },
+  Pluto : {
+    radius : 0.1,
+    distance : 21,
+    year : 6,
+    color : [ 0.6, 0.6, 0.6, 1.0 ]
+  }
+};
